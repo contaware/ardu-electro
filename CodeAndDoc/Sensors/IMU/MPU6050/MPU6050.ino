6799e53a96dbe6507eb63273fee9310f4bc69cdc
Oliver Pfister <oliver.pfister@contaware.com>
2022-06-14 21:07:52 +0000

    Updated MPU6050
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@133 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Sensors/IMU/MPU6050/MPU6050.ino
+++ CodeAndDoc/Sensors/IMU/MPU6050/MPU6050.ino
@@ -1,33 +1,34 @@
 /*
-  Gyro module with MPU-6050 chip
+  6-axis IMU module with MPU-6050 chip
 
   - VDD supply is 3.3V - 5.5V.
   - The chip runs at 3.3V.
   - Connecting AD0 to HIGH changes the I2C address from 0x68 to 0x69.
   - On the module the SDA and SCK lines have 2.2k pull-ups connected to 3.3V.
   - The module has no level shifters, better to use external ones.
+  - https://wolles-elektronikkiste.de/en/mpu6050-accelerometer-and-gyroscope
 */
 #include <Wire.h>
 
-const int MPU_addr = 0x68;  // I2C address of the MPU-6050
+const int MPU6050_ADDR = 0x68;  // I2C address of the MPU-6050
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 
 void setup()
 {
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
   Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU6050_ADDR);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero to wake up the MPU-6050
+  Wire.endTransmission(true);
 }
 
 void loop()
 {
-  Wire.beginTransmission(MPU_addr);
+  Wire.beginTransmission(MPU6050_ADDR);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart, as a result the connection is kept active
+  Wire.requestFrom(MPU6050_ADDR, 14, true);  // request a total of 7*2=14 registers
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
@@ -38,9 +39,9 @@ void loop()
   Serial.print("AcX = "); Serial.print(AcX);
   Serial.print(" | AcY = "); Serial.print(AcY);
   Serial.print(" | AcZ = "); Serial.print(AcZ);
-  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | Tmp = "); Serial.print((Tmp + 12412.0) / 340.0);  // equation for temperature in degrees C from datasheet
   Serial.print(" | GyX = "); Serial.print(GyX);
   Serial.print(" | GyY = "); Serial.print(GyY);
   Serial.print(" | GyZ = "); Serial.println(GyZ);
-  delay(333);
+  delay(1000);
 }
