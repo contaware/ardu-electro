08d1654e7cdf5d537879f42281e8db62841031f7
Oliver Pfister <oliver.pfister@contaware.com>
2022-06-16 13:21:15 +0000

    Documented both PCF8574 and PCF8574A
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@136 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Display/LCD_HD44780_I2C/LCD_HD44780_I2C.ino
+++ CodeAndDoc/Display/LCD_HD44780_I2C/LCD_HD44780_I2C.ino
@@ -10,9 +10,9 @@
 */
 #include <LiquidCrystal_I2C.h>
 
-const uint8_t LCD_COLS = 16;  // 16, 20
-const uint8_t LCD_ROWS = 2;   // 2,  4
-LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS); // set the LCD address to 0x27
+const uint8_t LCD_COLS = 16;  // 16 or 20
+const uint8_t LCD_ROWS = 2;   // 2  or 4
+LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS); // PCF8574: 0x20 - 0x27 (default), PCF8574A: 0x38 - 0x3F (default)
 
 // Custom chars 5 x 8 dots
 // https://arduinogetstarted.com/faq/how-to-use-special-character-on-lcd
