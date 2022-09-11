def4bb4ef6cc841e98aa4e646113a36c5da13e44
Oliver Pfister <oliver.pfister@contaware.com>
2022-09-11 08:52:43 +0000

    Update DHTxx Temp/Humid sensors with PrintCol.h
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@220 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Sensors/TempHumidPressure/DHTxx/DHTxx.ino
+++ CodeAndDoc/Sensors/TempHumidPressure/DHTxx/DHTxx.ino
@@ -1,5 +1,5 @@
 /*
-  DHT11 or DHT22
+  DHT11/DHT22 humidity and temperature sensor
 
   - VDD supply is 3.3V - 5.5V.
   
@@ -10,63 +10,66 @@
     the DHT11 humidity range is from 20% to 80% (5% accuracy).
 */
 #include <SimpleDHT.h>
+#include <PrintCol.h>
 
-const byte DHT_PIN = 2;
-
-#define DHT 22                  // define 11 or 22
+const byte DHT_PIN = 2;                         // set the signal PIN
+#define DHT 22                                  // choose type: 11 or 22
 
 #if DHT == 22
 SimpleDHT22 dht22(DHT_PIN);
-const int POLL_RATE_MS = 2500;  // DHT22 sampling rate is 0.5 HZ 
-                                // -> be above that otherwise an error is returned!
+const int TIME_BETWEEN_MEASUREMENTS_MS = 2500;  // DHT22 sampling rate is 0.5 HZ 
+                                                // -> be above that otherwise an error is returned!
 #else
 SimpleDHT11 dht11(DHT_PIN);
-const int POLL_RATE_MS = 1500;  // DHT11 sampling rate is 1 HZ 
-                                // -> be above that otherwise an error is returned!
+const int TIME_BETWEEN_MEASUREMENTS_MS = 1500;  // DHT11 sampling rate is 1 HZ 
+                                                // -> be above that otherwise an error is returned!
 #endif
-
-
 
 void setup()
 {
   Serial.begin(9600);
+
+#if DHT == 22
+  Serial.println("Read DHT22");
+#else
+  Serial.println("Read DHT11");
+#endif
 }
 
 void loop() 
 {
+  Serial.println("----------------------");
+  
 // Read float
 #if DHT == 22
-  Serial.println("=================================");
-  Serial.println("Sample DHT22...");
   float temperature = 0;
   float humidity = 0;
   int err = SimpleDHTErrSuccess;
   if ((err = dht22.read2(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
   {
-    Serial.print("Read DHT22 failed, err="); Serial.print(SimpleDHTErrCode(err));
-    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(POLL_RATE_MS);
+    Serial.print("DHT22 error="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(TIME_BETWEEN_MEASUREMENTS_MS);
     return;
   }
 // Read byte
 #else
-  Serial.println("=================================");
-  Serial.println("Sample DHT11...");
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
   {
-    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
-    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(POLL_RATE_MS);
+    Serial.print("DHT11 error="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(TIME_BETWEEN_MEASUREMENTS_MS);
     return;
   }
 #endif
 
-  // Print temperature and humidity
-  Serial.print("Sample OK: ");
-  Serial.print(temperature); Serial.print(" °C , ");
-  Serial.print(humidity); Serial.println(" RH%");
+  // Print humidity and temperature
+  Serial.println(" Humidity %   Temp. °C");
+  printCol(humidity);
+  printCol(temperature);
+  Serial.println();
   
   // Wait
-  delay(POLL_RATE_MS);
+  delay(TIME_BETWEEN_MEASUREMENTS_MS);
 }
