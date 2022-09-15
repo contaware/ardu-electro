6ee8b5499e138bd7323dd0a304999a39f604c7d1
Oliver Pfister <oliver.pfister@contaware.com>
2022-09-15 13:42:49 +0000

    Arduino 4 Relays Shield
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@223 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Relay/Relay.ino
+++ CodeAndDoc/Relay/Relay.ino
@@ -1,15 +1,40 @@
 /*
-  When disconnecting the JDVcc jumper from Vcc we can use a separate power source for the relay coils:
-  - As a normal rule, if an external power supply is used, it's important to short the Arduno ground with the external power ground.
-  - But for a "total opto isolation configuration" the Arduino ground should not be connected with the external power
-    ground (it works because the Arduino drives the optical coupler with an active LOW signal to the IN pin)
+ In this code with use the Arduino 4 Relays Shield
+
+ - Max load is 48V / 60W.
+ 
+ - Coil current consumption is 35mA for each relay.
+ 
+ - Note that there are many other shields or modules, for example some of the modules
+   have a JDVcc jumper which permits the use of a separate power source for the relay
+   coils, and some have also a "total opto isolation configuration" which allows us 
+   to not connect the Arduino ground with the external power ground (it works because
+   the Arduino drives the optical coupler with an active LOW signal to the IN pin). 
 */
-const byte RELAY_PIN = 7;
+const byte RELAY1_PIN = 4;
+const byte RELAY2_PIN = 7;
+const byte RELAY3_PIN = 8;
+const byte RELAY4_PIN = 12;
 
 void setup()
 {
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, LOW); // active LOW signal to the IN pin
+  // Pins default to inputs, set them LOW before switching to outputs
+  digitalWrite(RELAY1_PIN, LOW);
+  digitalWrite(RELAY2_PIN, LOW);
+  digitalWrite(RELAY3_PIN, LOW);
+  digitalWrite(RELAY4_PIN, LOW);
+  pinMode(RELAY1_PIN, OUTPUT);
+  pinMode(RELAY2_PIN, OUTPUT);
+  pinMode(RELAY3_PIN, OUTPUT);
+  pinMode(RELAY4_PIN, OUTPUT);
+
+  delay(5000);  
+
+  // All relays ON
+  digitalWrite(RELAY1_PIN, HIGH);
+  digitalWrite(RELAY2_PIN, HIGH);
+  digitalWrite(RELAY3_PIN, HIGH);
+  digitalWrite(RELAY4_PIN, HIGH);
 }
 
 void loop()
