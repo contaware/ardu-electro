cd11b835eb7d87b7bd19f3fe151c8dddb81d7fc2
Oliver Pfister <oliver.pfister@contaware.com>
2022-10-08 15:51:58 +0000

    Added header comment about multiple sensors usage
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@263 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Sensors/DistanceSensor_ToF/VL53L0X_NonBlocking_Single/VL53L0X_NonBlocking_Single.ino
+++ CodeAndDoc/Sensors/DistanceSensor_ToF/VL53L0X_NonBlocking_Single/VL53L0X_NonBlocking_Single.ino
@@ -4,6 +4,14 @@
   - VDD supply is 2.6V - 5.5V.
   
   - The violet VL53L0XV2 module has a 2.8V regulator and integrated level-shifters.
+
+  - The first time you use the module remove the protective plastic from the sensor.
+
+  - The default I2C address is VL53L0X_I2C_ADDR (0x29), it's possible to change it in
+    software through lox.begin(0x30). If using multiple sensors then each begin() has
+    to be called with the other sensors in shutdown mode (XSHUT LOW); wire all XSHUT 
+    pins to Arduino. Note that this has to be done every time the power is turned on
+    (the configured I2C addresses are not permanent).
 */
 #include "Adafruit_VL53L0X.h"
  
