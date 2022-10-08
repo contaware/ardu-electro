6d68ed600d12092bd5848b2fdf4a97af7a4352be
Oliver Pfister <oliver.pfister@contaware.com>
2022-10-08 08:07:08 +0000

    Set Serial.begin(115200) so that the serial communication is not too much delaying us
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@259 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/RobotCar/RC-Config.h
+++ CodeAndDoc/RobotCar/RC-Config.h
@@ -11,7 +11,7 @@
 // - if USE_DPRINT is set to true, DPRINT, DPRINTLN and DWRITE do output to Serial Monitor.
 // - if USE_DPRINT is set to false, DPRINT, DPRINTLN and DWRITE are optimized away.
 #define USE_DPRINT                true
-#define DPRINT_SERIAL_SPEED       9600
+#define DPRINT_SERIAL_SPEED       115200
 #define DPRINT(...)               do { if (USE_DPRINT) Serial.print(__VA_ARGS__); } while (false)
 #define DPRINTLN(...)             do { if (USE_DPRINT) Serial.println(__VA_ARGS__); } while (false)
 #define DWRITE(...)               do { if (USE_DPRINT) Serial.write(__VA_ARGS__); } while (false)
