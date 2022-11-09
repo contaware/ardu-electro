727070bad4787934b027d78bacff32b8aed2028f
Oliver Pfister <oliver.pfister@contaware.com>
2022-11-09 16:30:05 +0000

    Added zero current calibration function and found a nice paper about accuracy
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@306 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Energy/I_P_Measure/WCS1x00/WCS1x00.ino
+++ CodeAndDoc/Energy/I_P_Measure/WCS1x00/WCS1x00.ino
@@ -1,15 +1,16 @@
 /*
-  WCS1600, WCS1700 and WCS1800 current measurement through Hall Effect
+  Winson WCS1600, WCS1700 and WCS1800 current measurement through Hall Effect
  
   - VDD supply is 3V – 12V.
   
-  - In the WCS1x00 the current to be measured generates a magnetic field
-    which is converted into a voltage, that can be picked-up at the Out pin.
-    Due to its measuring principle, the WCS1x00 influences the current to be
-    measured less than a shunt-based module. However the precision is not
-    so good.
+  - The current to be measured generates a magnetic field which is converted 
+    into a voltage, that can be picked-up at the Out pin. Due to its measuring
+    principle, the WCS1x00 influences the current to be measured less than a 
+    shunt-based module. However the precision is not so good. Even if zero 
+    calibrated the sensors are not symmetrical: inverting the current direction
+    with the same load returns an absolute value which can also differ by 10%.
   
-  - 3 sensors:
+  - With 5V supply voltage (can be fine tuned with known test currents):
     WCS1800: 66mV/A and max 35A
     WCS1700: 33mV/A and max 70A
     WCS1600: 22mV/A and max 100A
@@ -17,46 +18,64 @@
   - Vout = VDD/2 at 0A and changes by the specified amount of mV/A
     For example for the WCS1800 sensor:
     Vout = 0.066 * I + VDD/2  ->  I = (Vout - VDD/2) / 0.066​
+
+  - There is a WinsonLib which does what's done here.
 */
 #include <PrintCol.h>
  
 const byte WCS1x00_PIN = A0;
-const int zeroCurrent = 533;  // test that!
-const float VperA = 0.066;    // set that according to the used sensor
-const float factor = 5.0 / (1023.0 * VperA);
-float currentAvg = 0.0;
-#define SERIAL_PLOTTER   false
+int zeroCurrent = 512;      // zeroCalibrate() inits that
+const float VperA = 0.066;  // set according to the used sensor
+const float factorCurrent = 5.0 / (1023.0 * VperA);
+const int CALIB_SIZE = 120;
+float avgCurrent = 0.0;
+#define SERIAL_PLOTTER      false
 
+void zeroCalibrate()
+{
+  analogRead(WCS1x00_PIN);  // read analog pin to stabilize the ADC
+  uint32_t startMicros;
+  int32_t dataSum = 0;
+  for (int i = 0; i < CALIB_SIZE; i++)
+  {
+    startMicros = micros();
+    dataSum += analogRead(WCS1x00_PIN);
+    while (micros() - startMicros < 829); // sample rate = 1200Hz
+  }
+  zeroCurrent = dataSum / CALIB_SIZE;
+  Serial.print("zeroCurrent: ");
+  Serial.println(zeroCurrent);
+}
+  
 void setup()
 {
   Serial.begin(115200);
   Serial.println("WCS1x00 Test");
-  Serial.println();
+  zeroCalibrate();
 }
 
 void loop()
 {
-  int raw = analogRead(WCS1x00_PIN);
+  int rawCurrent = analogRead(WCS1x00_PIN);
 
   // Instantaneous
-  //    current = (raw - zeroCurrent) * 5.0 / 1023 / VperA;
-  float current = (raw - zeroCurrent) * factor;
+  float instCurrent = (rawCurrent - zeroCurrent) * factorCurrent;
 
   // Moving average
-  currentAvg = (127 * currentAvg + current) / 128;
+  avgCurrent = (15 * avgCurrent + instCurrent) / 16;
   
 #if SERIAL_PLOTTER == true
-  Serial.println(current);
+  Serial.println(instCurrent);
 #else
   Serial.print("Instantaneous: ");
-  printCol(raw, DEC, 4);
+  printCol(rawCurrent, DEC, 4);
   Serial.print(" -> ");
-  printCol(current, 2, 6);
+  printCol(instCurrent, 2, 6);
   Serial.print("A");
   Serial.print("  |  Average: ");
-  printCol(currentAvg, 2, 6);
+  printCol(avgCurrent, 2, 6);
   Serial.println("A");
 #endif
 
-  delay(30);        
+  delay(50);        
 }
