0dd8a95b7d589b1e8a45ece34bcdfd3508a2415d
Oliver Pfister <oliver.pfister@contaware.com>
2023-01-17 12:00:48 +0000

    Created an architectures #define test sketch
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@367 8294280c-25f7-9146-af84-bcdaab6def61

---
+++ CodeAndDoc/System/Defines/Defines.ino
@@ -0,0 +1,60 @@
+/*
+  Check the architecture #define for your used board
+*/
+
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
+                    // that waits here until the user opens the Serial Monitor!
+
+  // All AVRs: traditional + megaAVR
+  #if defined(__AVR__)
+    Serial.println(F("__AVR__"));
+  #endif
+
+  // Traditional AVRs (UNO, Nano, Mega, Leonardo, ...) 
+  #if defined(ARDUINO_ARCH_AVR)
+    Serial.println(F("ARDUINO_ARCH_AVR"));
+  #endif
+
+  // megaAVRs (UNO WiFi rev2 and Nano Every)
+  #if defined(ARDUINO_ARCH_MEGAAVR)
+    Serial.println(F("ARDUINO_ARCH_MEGAAVR"));
+  #endif
+
+  // Samd21 (Zero, MKR, Nano 33)
+  #if defined(ARDUINO_ARCH_SAMD)
+    Serial.println(F("ARDUINO_ARCH_SAMD"));
+  #endif
+
+  // Sam (Due)
+  #if defined(ARDUINO_ARCH_SAM)
+    Serial.println(F("ARDUINO_ARCH_SAM"));
+  #endif
+
+  // ESP8266
+  #if defined(ARDUINO_ARCH_ESP8266)
+    Serial.println(F("ARDUINO_ARCH_ESP8266"));
+  #endif
+
+  // ESP32
+  #if defined(ARDUINO_ARCH_ESP32)
+    Serial.println(F("ARDUINO_ARCH_ESP32"));
+  #endif
+
+  // STM32
+  #if defined(ARDUINO_ARCH_STM32)
+    Serial.println(F("ARDUINO_ARCH_STM32"));
+  #endif
+
+  // RP2040
+  #if defined(ARDUINO_ARCH_RP2040)
+    Serial.println(F("ARDUINO_ARCH_RP2040"));
+  #endif
+}
+
+void loop()
+{
+
+}
