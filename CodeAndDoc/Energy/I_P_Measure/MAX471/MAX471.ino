3af524bf79546280a5161e37b81ce7131fb5c7d6
Oliver Pfister <oliver.pfister@contaware.com>
2023-02-16 22:37:37 +0000

    Added MAX471
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@403 8294280c-25f7-9146-af84-bcdaab6def61

---
+++ CodeAndDoc/Energy/I_P_Measure/MAX471/MAX471.ino
@@ -0,0 +1,35 @@
+/*
+  MAX471 current measurement through an internal shunt
+
+  - The MAX471 takes the power required for itself from the circuit to 
+    be measured, which must be in the range of 3V - 36V. This means that
+    RS+/RS- must be connected to the high side of the load.
+  
+  - It measures up to 3A (MAX471 power consumption < 100μA).
+  
+  - The shunt is of 35mΩ, with a 2kΩ resistor from OUT to GND the voltage
+    seen at OUT is 1V/A; this voltage is always positive regardless of the
+    current direction. The open-collector SIGN output indicates the current
+    flow direction.
+*/
+const byte OUT_PIN = A0;
+const byte SIGN_PIN = 8;
+
+void setup()
+{
+  pinMode(SIGN_PIN, INPUT_PULLUP);
+  Serial.begin(9600);
+  Serial.println("MAX471 Test");
+  Serial.println();
+}
+
+void loop()
+{
+  int sign = digitalRead(SIGN_PIN);
+  int out = analogRead(OUT_PIN);
+  float current = out * 5.0 / 1.023;
+  Serial.write(sign ? '+' : '-');
+  Serial.print(current);
+  Serial.println("mA");
+  delay(1000);       
+}
