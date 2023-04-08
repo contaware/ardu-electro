c1553091cee27b84c9bf664578c0b08b7a6f06bd
Oliver Pfister <oliver.pfister@contaware.com>
2023-04-08 11:00:05 +0000

    Small updates to the TTP223 sketch
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@495 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/UserInput/TouchSensor_TTP223/TouchSensor_TTP223.ino
+++ CodeAndDoc/UserInput/TouchSensor_TTP223/TouchSensor_TTP223.ino
@@ -2,8 +2,10 @@
   Touch Sensor with TTP223-B chip
 
   - VDD supply is 2.0V - 5.5V.
+  
   - Returns 1 as long as the finger is close to the sensor 
     (works also through non-metallic surfaces).
+    
   - The nice thing is that it is already debounced!
 */
 const byte TOUCH_PIN = 9;
@@ -13,6 +15,8 @@ int previousSensorValue = -1;
 void setup()
 {
   Serial.begin(9600);
+  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
+                    // that waits here until the user opens the Serial Monitor!
   pinMode(TOUCH_PIN, INPUT);
 }
 
