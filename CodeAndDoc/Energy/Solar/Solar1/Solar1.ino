a2d9b9533d526a35f1b0f3ebff1d650eda10fab9
Oliver Pfister <oliver.pfister@contaware.com>
2023-04-17 21:11:56 +0000

    Added sensors to the Solar1 project
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@507 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Energy/Solar/Solar1/Solar1.ino
+++ CodeAndDoc/Energy/Solar/Solar1/Solar1.ino
@@ -6,42 +6,73 @@
     be an unused 3.3V output on the 3Vo pin). Data means I2C SDA and Clk is I2C SCL.
     
   - The TTP223 touch button is already debounced.
+
+  - DHT22 sampling rate is 0.5 Hz -> poll slower than that, otherwise an error is
+    returned.
+
+  - The Guva-S12SD sensor covers all UVB and most of the UVA spectrum
+    (240nm - 370nm).
 */
 #include "TimerPoll.h"
+#include "PrintCol.h"
+#include <SimpleDHT.h>
 
 // Touch button
 const byte TOUCH_PIN = 13;
 int previousTouchValue = -1;
-unsigned long lastTouchPressMillis;
+unsigned long lastTouchMillis;
 const unsigned long TOUCH_POLL_MS = 100;
 TimerPoll timerTouch;
+
+// Solar voltage (using a 1/2 voltage divider)
+const byte SOLAR_PIN = A2;
+
+// Battery voltage
+const byte BATTERY_PIN = A3;
+
+// Uv
+const byte UV_PIN = A0;
+
+// DHT22 humidity and temperature sensor
+const byte DHT_PIN = 8;
+SimpleDHT22 dht22(DHT_PIN);
+float temperature = 0.0;
+float humidity = 0.0;
+const unsigned long TEMP_HUMID_POLL_MS = 2543;
+TimerPoll timerTempHumid;
+
+// UpTime
+uint64_t currentUpTime64 = 0;
+const unsigned long SEC_TICK_MS = 1000;
+TimerPoll timerSecTick;
 
 // Oled
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH            128         // OLED display width, in pixels, usually 128
-#define SCREEN_HEIGHT           64          // OLED display height, in pixels, usually 64 or 32
-#define SCREEN_ADDRESS          0x3D        // see board for Address: 0x3C or 0x3D
-#define SSD1306_STARTUP_MS      500         // SSD1306 needs a small amount of time to be ready after initial power
+#define SCREEN_WIDTH            128             // OLED display width, in pixels, usually 128
+#define SCREEN_HEIGHT           64              // OLED display height, in pixels, usually 64 or 32
+#define SCREEN_ADDRESS          0x3D            // see board for Address: 0x3C or 0x3D
+#define SSD1306_STARTUP_MS      500             // SSD1306 needs a small amount of time to be ready after initial power
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); // for STEMMA QT the RST pin is not necessary, so we pass -1
+int displayPage = 0;
+const int DISPLAY_PAGES = 3;
 bool oledIsOn = false;
 const unsigned long OLED_OFF_TIMEOUT_MS = 60000;
-const unsigned long DISPLAY_RATE_MS = 1000;
-uint64_t currentUpTime64 = 0;
-TimerPoll timerDisplay;
-
-// Battery voltage
-const byte BATTERY_PIN = A3;
 
 void setup()
 {
   // Touch button
   pinMode(TOUCH_PIN, INPUT);
   timerTouch.begin(TOUCH_POLL_MS, touchPoll);
-  lastTouchPressMillis = millis();
+  lastTouchMillis = millis();
 
+  // UpTime
+  timerSecTick.begin(SEC_TICK_MS, onSecTick);
+  
+  // Temperature and humidity
+  timerTempHumid.begin(TEMP_HUMID_POLL_MS, tempHumidPoll);
+  
   // Oled
-  timerDisplay.begin(DISPLAY_RATE_MS, doDisplay);
   /*
     When powering the device if your code tries to write to the display too soon,
     it just shows a black screen (oled.begin() succeeds but nothing works).
@@ -54,76 +85,135 @@ void setup()
   oled.display();
   oled.ssd1306_command(SSD1306_DISPLAYON);
   oledIsOn = true;
-  oled.cp437(true);                         // use the 'Code Page 437'-compatible charset
-  oled.setTextColor(SSD1306_WHITE);         // draw white text (must be called)
-  oled.setCursor(0, 0);                     // start at top-left corner
+  oled.cp437(true);                             // use the 'Code Page 437'-compatible charset
+  oled.setTextColor(SSD1306_WHITE);             // draw white text (must be called)
+  oled.setTextSize(2);                          // draw 2X-scale text
 }
 
-void printUpTime(uint64_t upTime64)
-{
-  const uint64_t millisPerDay = 24ULL * 60 * 60 * 1000;
-  int elapsedDays = (int)(upTime64 / millisPerDay);
-  upTime64 = upTime64 % millisPerDay;
-  
-  const uint64_t millisPerHour = 60ULL * 60 * 1000;
-  int elapsedHours = (int)(upTime64 / millisPerHour);
-  upTime64 = upTime64 % millisPerHour;
-  
-  const uint64_t millisPerMin = 60ULL * 1000;
-  int elapsedMin = (int)(upTime64 / millisPerMin);
-  upTime64 = upTime64 % millisPerMin;
-  
-  const uint64_t millisPerSec = 1000ULL;
-  int elapsedSec = (int)(upTime64 / millisPerSec);
-  upTime64 = upTime64 % millisPerSec;
-
-  char s[16];
-  sprintf(s, "%d:%02d:%02d:%02d", elapsedDays, elapsedHours, elapsedMin, elapsedSec);
-  oled.println(s);
-}
-
-void doDisplay()
+void displayPage0()
 {
   oled.clearDisplay();
   oled.setCursor(0, 0);
-  
-  // Display Up Time
-  oled.setTextSize(2);                      // draw 2X-scale text
-  oled.println("UpTime");
-  oled.setTextSize(1);                      // restore default 1:1 pixel scale
-  currentUpTime64 += DISPLAY_RATE_MS;       // doDisplay() calls are in sync with the millis() counter, no drift!
-  printUpTime(currentUpTime64);
 
-  // Display Battery voltage
-  oled.setTextSize(2);                      // draw 2X-scale text
-  oled.println("Battery");
-  oled.setTextSize(1);                      // restore default 1:1 pixel scale
-  float voltage = 5.0 * analogRead(BATTERY_PIN) / 1023.0;
-  oled.print(voltage);
+  // Solar voltage
+  float voltage = 10.0 * analogRead(SOLAR_PIN) / 1023.0;
+  oled.print("Pv");
+  printCol(voltage, 2, 7, oled);
   oled.println("V");
+  
+  // Battery voltage
+  voltage = 5.0 * analogRead(BATTERY_PIN) / 1023.0;
+  oled.print("Bat");
+  printCol(voltage, 2, 6, oled);
+  oled.println("V");
+
+  // Temperature
+  oled.print("T");
+  printCol(temperature, 1, 7, oled);
+  oled.write(0xF8); // cp437 code for °
+  oled.println("C");
+
+  // Relative humidity
+  oled.print("Rh");
+  printCol(humidity, 1, 7, oled);
+  oled.println("%");
   
   oled.display();
 }
 
+void displayPage1()
+{
+  oled.clearDisplay();
+  oled.setCursor(0, 0);
+
+  oled.print("Uv");
+  printCol(50.0 * analogRead(UV_PIN) / 1023.0, 1, 8, oled);
+  oled.println();
+  oled.setTextSize(1);
+  oled.println();
+  oled.println("0..2    low");
+  oled.println("3..5    medium");
+  oled.println("6..7    high");
+  oled.println("8..10   very high");
+  oled.println(">=11    stay inside!");
+  oled.setTextSize(2);
+  
+  oled.display();
+}
+
+void displayPage2()
+{
+  oled.clearDisplay();
+  oled.setCursor(0, 0);
+
+  uint64_t upTime64 = currentUpTime64; 
+  const uint64_t millisPerDay = 24ULL * 60 * 60 * 1000;
+  int elapsedDays = (int)(upTime64 / millisPerDay);
+  upTime64 %= millisPerDay;
+  
+  const uint64_t millisPerHour = 60ULL * 60 * 1000;
+  int elapsedHours = (int)(upTime64 / millisPerHour);
+  upTime64 %= millisPerHour;
+  
+  const uint64_t millisPerMin = 60ULL * 1000;
+  int elapsedMin = (int)(upTime64 / millisPerMin);
+  upTime64 %= millisPerMin;
+  
+  const uint64_t millisPerSec = 1000ULL;
+  int elapsedSec = (int)(upTime64 / millisPerSec);
+  upTime64 %= millisPerSec;
+
+  oled.print("Days");
+  printCol(elapsedDays, DEC, 6, oled);
+  oled.println();
+
+  oled.print("Hours");
+  printCol(elapsedHours, DEC, 5, oled);
+  oled.println();
+  
+  oled.print("Min");
+  printCol(elapsedMin, DEC, 7, oled);
+  oled.println();
+  
+  oled.print("Sec");
+  printCol(elapsedSec, DEC, 7, oled);
+  oled.println();
+  
+  oled.display();
+}
+
+void displayCurrentPage()
+{
+  switch (displayPage)
+  {
+    case 0:   displayPage0(); break;
+    case 1:   displayPage1(); break;
+    default:  displayPage2(); break;
+  }
+}
+
 void touchPoll()
 {
+  bool hasTurnedOn = false;
   int touchValue = digitalRead(TOUCH_PIN);
   unsigned long currentMillis = millis();
 
-  // Display ON
+  // Turn display ON?
   if (touchValue == 1)
   {
-    lastTouchPressMillis = currentMillis;
+    lastTouchMillis = currentMillis;
     if (!oledIsOn)
     {
       oled.ssd1306_command(SSD1306_DISPLAYON);
       oledIsOn = true;
+      hasTurnedOn = true;
+      displayCurrentPage();
     }
   }
   else
   {
-    // Display OFF
-    if (currentMillis - lastTouchPressMillis > OLED_OFF_TIMEOUT_MS)
+    // Turn display OFF?
+    if (currentMillis - lastTouchMillis > OLED_OFF_TIMEOUT_MS)
     {
       if (oledIsOn)
       {
@@ -136,20 +226,31 @@ void touchPoll()
   // Handle touch change
   if (previousTouchValue != touchValue)
   {
-    if (touchValue == 1)
+    if (touchValue == 1 && !hasTurnedOn)
     {
-      // OFF -> ON
-    }
-    else
-    { 
-      // ON -> OFF
+      displayPage++;
+      displayPage %= DISPLAY_PAGES;
+      displayCurrentPage();
     }
     previousTouchValue = touchValue;
   }
 }
 
+void onSecTick()
+{
+  currentUpTime64 += SEC_TICK_MS; // onSecTick() calls are in sync with the millis() counter, no drift!
+  if (oledIsOn)
+    displayCurrentPage();
+}
+
+void tempHumidPoll()
+{
+  dht22.read2(&temperature, &humidity, NULL);
+}
+
 void loop()
 {
   timerTouch.process();
-  timerDisplay.process();
+  timerSecTick.process();
+  timerTempHumid.process();
 }
