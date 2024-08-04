a1b79fff6f6dcd4553f8209d6de7b16d9a141925
Oliver Pfister <oliver.pfister@contaware.com>
2024-08-04 21:10:14 +0000

    Add SerialPlotter sketch
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@806 8294280c-25f7-9146-af84-bcdaab6def61

---
+++ CodeAndDoc/Communication/Serial/SerialPlotter/SerialPlotter.ino
@@ -0,0 +1,65 @@
+/*
+  Serial Plotter
+
+  - The vertical Y axis auto adjusts as the value of the output increases 
+    or decreases, and the X axis is fixed at 500 points in IDE 1.x and at
+    50 points in IDE 2.x. When reaching the X axis limit the oldest points 
+    are dropped, the visual effect is the graph scrolling out to the left.
+    
+  - Each tick of the axis equals to an executed serial println() command,
+    there is no time scale possibility.
+
+  - To plot multiple values do separate them by a space, comma or \t. 
+
+  - To print labels prepend the values with a label text followed by a 
+    colon. Cannot use colon, space, comma or \t in label text.
+*/
+
+const int halfPeriod = 70;          // number of sample for a half-period
+const bool noYAutoScaling = false;  // set to true to avoid auto-scaling Y axis
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
+                    // that waits here until the user opens the Serial Monitor!
+  delay(5000);      // for ESP32 and some other MCUs a delay() is needed, otherwise
+                    // the messages generated in setup() can't be seen!
+}
+
+void loop()
+{
+  static int count = 0;
+  static int addValue = 1;
+
+  // Print values
+  if (noYAutoScaling)
+  {
+    Serial.print("Max:");
+    Serial.print(halfPeriod);
+    Serial.print(",");
+  }
+  Serial.print("My-Label1:");
+  Serial.print(count);
+  Serial.print(",");
+  Serial.print("My-Label2:");
+  Serial.print(-count);
+  if (noYAutoScaling)
+  {
+    Serial.print(",Min:");
+    Serial.print(-halfPeriod);
+  }
+  Serial.println();
+  
+  // Inc/Dec
+  count += addValue;
+
+  // What to do in next round
+  if (count >= halfPeriod)
+    addValue = -1;
+  else if (count <= 0)
+    addValue = 1;
+
+  // Delay
+  delay(80);
+}
