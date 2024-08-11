3c6580fd92f3101a20060153c28e7a29b45535da
Oliver Pfister <oliver.pfister@contaware.com>
2024-08-11 12:26:43 +0000

    Update ball switch sketch
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@821 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Sensors/BallSwitch/BallSwitch.ino
+++ CodeAndDoc/Sensors/BallSwitch/BallSwitch.ino
@@ -1,18 +1,23 @@
+/*
+ Ball switch which opens when tilted
+ 
+ - Connect one end to GND and the other to SWITCH_PIN.
+
+ - Debounce if using as digital signal!
+*/
+
+const byte SWITCH_PIN = 8;
+
 void setup()
 { 
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(2, INPUT_PULLUP); // the following is also working, but not so understandable: pinMode(2, INPUT); digitalWrite(2, HIGH);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
 } 
 
 void loop() 
-{  
-  int digitalVal = digitalRead(2);
-  if (HIGH == digitalVal)
-  {
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-  else
-  {
+{
+  if (digitalRead(SWITCH_PIN) == LOW)
     digitalWrite(LED_BUILTIN, HIGH);
-  }
+  else
+    digitalWrite(LED_BUILTIN, LOW);
 }
