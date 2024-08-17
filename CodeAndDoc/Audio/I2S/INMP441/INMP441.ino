ed657ae350a4dfb8f57b5e2f744f7de5ae2dbec1
Oliver Pfister <oliver.pfister@contaware.com>
2024-08-17 20:56:04 +0000

    Add INMP441 I2S mic module
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@836 8294280c-25f7-9146-af84-bcdaab6def61

---
+++ CodeAndDoc/Audio/I2S/INMP441/INMP441.ino
@@ -0,0 +1,67 @@
+/*
+  INMP441 is a mono I2S Microphone, view the audio on the Serial Plotter
+
+  - VDD (3V pin) is 1.6V - 3.6V.
+
+  - The microphone hole is on the bottom side.
+
+  - The data format is 2's complement at 24-bit.
+
+  - When the select channel pin (L/R) is low, the microphone will transmit on
+    the left channel. Connecting this pin to VDD will instruct the chip to 
+    transmit on the right channel.
+    It's possible to use two microphones by sharing SCK, WS and SD; 
+    one would have L/R tied to GND and the other one to VDD.
+    
+  - WS/FS connect to pin 0 (Zero) or pin 3 (MKR) or A2 (Nano 33 IoT) or D8 (Nano ESP32).
+    SCK   connect to pin 1 (Zero) or pin 2 (MKR) or A3 (Nano 33 IoT) or D7 (Nano ESP32).
+    SD    connect to pin 9 (Zero) or pin A6 (MKR) or 4 (Nano 33 IoT) or D9 (Nano ESP32).
+*/
+
+#include <I2S.h>
+
+unsigned int goodSamplesCount = 0;
+
+void setup()
+{
+  // A baud rate of 115200 is used instead of 9600 for a faster data rate
+  Serial.begin(115200);
+  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
+                    // that waits here until the user opens the Serial Monitor!
+  delay(5000);      // for ESP32 and some other MCUs a delay() is needed, otherwise
+                    // the messages generated in setup() can't be seen!
+
+  // Start I2S at 8 kHz with 32-bits per sample
+  if (!I2S.begin(I2S_PHILIPS_MODE, 8000, 32))
+  {
+    Serial.println("Failed to initialize I2S!");
+    while (true);
+  }
+}
+
+void loop()
+{
+  // Read a sample
+  int sample = I2S.read();
+   
+  // Unfortunatelly there is no way to know from which channel a sample comes,
+  // filter-out the unused channel like:
+  if (sample && sample != -1 && sample != 1)
+  {
+    // Convert to 24-bit signed
+    sample >>= 8;
+    
+    // Only print each 40 samples to avoid overflowing the serial port
+    if ((goodSamplesCount % 40) == 0)
+    {
+      Serial.print("h:400000,s:");
+      Serial.print(sample);
+      Serial.println(",l:-400000");
+    }
+
+    // Inc. samples count
+    // Note: if it overflows there is no problem because it is only used above
+    //       with the modulo operator.
+    goodSamplesCount++;
+  }
+}
