9630e85b66a4cc4d52f58cc570bbd799856a4e2d
Oliver Pfister <oliver.pfister@contaware.com>
2024-08-20 11:35:55 +0000

    Fix indentation
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@843 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Audio/BuzzerActive/BuzzerActive.ino
+++ CodeAndDoc/Audio/BuzzerActive/BuzzerActive.ino
@@ -7,21 +7,21 @@ void setup()
 
 void loop()
 {
-    // Output a frequency
-    for (int i = 0 ; i < 80 ; i++)
-    {
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(1); // wait for 1ms
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(1); // wait for 1ms
-    }
+  // Output a frequency
+  for (int i = 0 ; i < 80 ; i++)
+  {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(1); // wait for 1ms
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(1); // wait for 1ms
+  }
 
-    // Output another frequency
-    for (int i = 0 ; i < 100 ; i++)
-    {
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(2); // wait for 2ms
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(2); // wait for 2ms
-    }
+  // Output another frequency
+  for (int i = 0 ; i < 100 ; i++)
+  {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(2); // wait for 2ms
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(2); // wait for 2ms
+  }
 } 
