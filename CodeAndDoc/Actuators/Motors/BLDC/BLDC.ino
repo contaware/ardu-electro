14b88f720742bbb0932c7ec7b2fa78caf2478d07
Oliver Pfister <oliver.pfister@contaware.com>
2024-10-06 15:56:34 +0000

    Add BLDC
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1022 8294280c-25f7-9146-af84-bcdaab6def61

---
+++ CodeAndDoc/Actuators/Motors/BLDC/BLDC.ino
@@ -0,0 +1,56 @@
+/* 
+  Drive a BLDC (Brushless DC motor) with an ESC (Electronic Speed Controller)
+
+  - An ESC has always three control wires, the middle pin is usually red and it 
+    outputs 5V, never wire that one when connection the Arduino to the USB
+    port or if powering the Arduino from another source! Connect GND (black or brown)
+    to the Arduino GND and the signal wire to the Arduino ESC_PIN defined below.
+  
+  - Most ESCs must be armed before being used. The arming procedure depends from
+    the ESC model. Bidirectional ESCs are usually armed in middle position (90°),
+    while monodirectional ESCs are armed by setting the position to 0°.
+*/
+#include <Servo.h>
+
+#define ESC_PIN       9
+#define POT_PIN       A0
+
+Servo ESC;
+unsigned long lastMillis;
+
+void setup()
+{
+  // Init Serial
+  Serial.begin(9600);
+  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
+                    // that waits here until the user opens the Serial Monitor!
+  delay(5000);      // for ESP32 and some other MCUs a delay() is needed, otherwise
+                    // the messages generated in setup() can't be seen!
+
+  Serial.println("Drive a BLDC with an ESC");
+
+  // Init min and max, each ESC is a bit different, test it!
+  ESC.attach(ESC_PIN,
+            1000,   // default is MIN_PULSE_WIDTH=544us
+            2000);  // default is MAX_PULSE_WIDTH=2400us
+
+  // Init millis var
+  lastMillis = millis();
+}
+
+void loop()
+{
+  // Regulate
+  int potVal = analogRead(POT_PIN);
+  int val = map(potVal, 0, 1023, 0, 180);
+  ESC.write(val);
+
+  // Serial Print
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastMillis > 100)
+  {
+    lastMillis = currentMillis;
+    Serial.print(ESC.readMicroseconds());
+    Serial.println("us");
+  }
+}
