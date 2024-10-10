e1d1e3c031ca6d92fe1ab7f0a3e2f28aa2e6a49d
Oliver Pfister <oliver.pfister@contaware.com>
2024-10-10 06:47:04 +0000

    Add TB67H450FNG module
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1027 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Actuators/Motors/MotorDC/MotorDC_2pinDrive/MotorDC_2pinDrive.ino
+++ CodeAndDoc/Actuators/Motors/MotorDC/MotorDC_2pinDrive/MotorDC_2pinDrive.ino
@@ -1,26 +1,33 @@
 /* 
   Drive a DC motor with two Arduino pins
 
-  A. Using the TB67H450FNG/A4950/AT8236 driver
+  A. Using the TB67H450FNG/A4950/AT8236 drivers
   
-  - The TB67H450FNG/A4950/AT8236 chip has one H-bridges with no 
+  - The TB67H450FNG/A4950/AT8236 chips have one H-bridges with no 
     ENABLE/PWM pin. IN1 and IN2 are logic input signals between 3.3-5.25V.
 
   - The MOSFET driver's body diodes are usually enough to protect the 
     MOSFET from the reverse voltage peak that occurs across the motor 
     coil when switching the motor off.
 
+  - The TB67H450FNG/A4950/AT8236 chips have a current limiting circuitry 
+    which works through a sense resistor attached between the RS pin and GND. 
+    Max current is calculated like: 
+    Iout(max) = VREF / Rsense / 10 = 3.3 / 0.250 / 10 = 1.3A
+
                      ----u----
                 GND |1       8| OUT2
     IN2_PIN <-> IN2 |2       7| RS <-> Rsense(0.250Ω) <-> GND
     IN1_PIN <-> IN1 |3       6| OUT1
-        5V <-> VREF |4       5| VCC motor (5.5-36V)
+      3.3V <-> VREF |4       5| VM (VCC motor)
                      ---------
             TB67H450FNG/A4950/AT8236 (3A)
 
-    Iout(max) = VREF / Rsense / 10 = 5 / 0.25 / 10 = 2A
+    TB67H450FNG:  VREF=0-4V        VM=4.5-44V
+    A4950:        VREF=0-5V        VM=8-40V
+    AT8236:       VREF=0.5-4V      VM=5.5-36V
 
-  
+
   B. Single transistor vs half H-bridge vs full H-bridge
 
   - A single transistor in low-side or high-side configuration is the simplest
