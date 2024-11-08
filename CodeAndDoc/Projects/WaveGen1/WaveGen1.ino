416615e5d3cc91275d3e5b4147db515befa84ba4
Oliver Pfister <oliver.pfister@contaware.com>
2024-11-08 12:48:18 +0000

    Change to RobTillaart's library
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1086 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Projects/WaveGen1/WaveGen1.ino
+++ CodeAndDoc/Projects/WaveGen1/WaveGen1.ino
@@ -2,30 +2,30 @@
   My waveform generator 1 is based on a AD9833 module, a 16x2 LCD with HD44780 chip and 
   PCF8574 I2C expander and a rotary encoder
  
-  - VDD supply of the Arduino board is 7V - 12V. There is a negative voltage module for 
-    the opamp and the buffer. The waveform generator works with the 5V of the USB, but 
-    then the opamp positive supply is a bit low and that could distort the positive wave 
-    when at maximum output level.
+  - There is an external +12V and -12V supply. Because of the Arduino power source 
+    selection circuitry, it's safe to plug the USB cable while there is power at
+    Vin from the external supply.
 
-  - The AD9833 module outputs the Sine and the Triangle waves at 0.6Vpp, the signal gets 
-    amplified by a LM318N opamp and buffered with a BUF634. We do not support the square 
-    wave output because it swings the full range from ground to VDD. Note that the output
-    amplitude decreases a bit with higher frequencies because of the AD9833 module itself
-    and also because of the bandwidth limit of the LM318N opamp.
+  - The AD9833 module outputs the Sine and the Triangle waves between GND and +0.6V, the
+    signal gets down-shifted (centered around GND) and amplified by the LM318N opamp and 
+    buffered with a BUF634. We do not support the square wave output because it swings 
+    the full range from ground to VDD. Note that the output amplitude decreases a bit 
+    with higher frequencies because of the AD9833 module itself and also because of the 
+    bandwidth limit of the LM318N opamp.
 
-  - To have a nice waveform the frequency is limited to 1 MHz (both AD9833 and LM318N are
-    the limiting factors).
+  - To have a nice waveform the frequency is limited in software to 1 MHz (both AD9833 
+    and LM318N are the limiting factors).
 */
-#include <AD9833.h> // download from https://github.com/Billwilliams1952/AD9833-Library-Arduino
+#include <AD9833.h>                 // https://github.com/RobTillaart/AD9833
 #include "RotaryEncoderPoll.h"
 #include "ButtonPoll.h"
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>      // LiquidCrystal I2C by Frank de Brabander, https://github.com/johnrickman/LiquidCrystal_I2C
 
 const long MAX_FREQ = 1000000;      // maximum adjustable frequency
 long freqHz = 0;                    // init frequency from 0 to MAX_FREQ
-WaveformType waveType = SINE_WAVE;  // init wave form to SINE_WAVE or TRIANGLE_WAVE
+uint8_t waveType = AD9833_SINE;     // init wave form to AD9833_SINE or AD9833_TRIANGLE
 const byte AD9833_FNC_PIN = 10;     // can be any digital IO pin
-AD9833 gen(AD9833_FNC_PIN);         // defaults to 25 MHz internal reference frequency
+AD9833 gen(AD9833_FNC_PIN);
 
 RotaryEncoderPoll enc;
 ButtonPoll btn;
@@ -40,15 +40,25 @@ const byte ROTARY_ENCODER_SW_PIN = 7;
 const uint8_t LCD_COLS = 16;
 const uint8_t LCD_ROWS = 2;
 LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS); // PCF8574: 0x20 - 0x27 (default), PCF8574A: 0x38 - 0x3F (default)
-const char arrow_up_char[] = {0b00100, 0b01110, 0b11111, 0b00100, 0b00100, 0b00100, 0b00100, 0b00100};  // custom char (5 x 8 dots)
+uint8_t arrow_up_char[] = {0b00100, 0b01110, 0b11111, 0b00100, 0b00100, 0b00100, 0b00100, 0b00100}; // custom char (5 x 8 dots)
+                                                                                                    // the library wants uint8_t and not const...
+
+void midscaleOut()
+{
+  // Set the AD9833 in RESET state which corresponds to a midscale output.
+  // Note: calling gen.setWave(AD9833_OFF) would result in a negative output voltage.
+  gen.writeControlRegister(1 << 8);
+}
 
 void setup()
-{   
+{
   // Init wave form generator module
-  gen.Begin();  // this MUST be the first command after declaring the AD9833 object
-  gen.ApplySignal(waveType, REG0, freqHz);
-  if (freqHz > 0)
-    gen.EnableOutput(true); // by default the output is disabled
+  SPI.begin();
+  gen.begin();
+  gen.setWave(waveType);
+  gen.setFrequency(freqHz);
+  if (freqHz == 0)
+    midscaleOut();
 
   // Init rotary encoder with button
   enc.begin(ROTARY_ENCODER_CLK_PIN, ROTARY_ENCODER_DT_PIN);
@@ -66,13 +76,13 @@ void lcdUpdate()
 {
   // First row
   lcd.setCursor(0, 0);
-  if (waveType == SINE_WAVE)
+  if (waveType == AD9833_SINE)
     lcd.print("Sine");                                          // this must be 4 chars
   else
     lcd.print("Tria");                                          // this must be 4 chars
   char s[13];
   char sFreq[8];
-  dtostrf(gen.GetActualProgrammedFrequency(REG0), 7, 0, sFreq); // this is exactly 7 chars
+  dtostrf(gen.getFrequency(), 7, 0, sFreq);                     // this is exactly 7 chars
   sprintf(s, "  %s Hz", sFreq);                                 // this is exactly 12 chars
   lcd.print(s);
 
@@ -123,18 +133,17 @@ void loop()
       {
         long newFreqHz = freqHz - pow10(pos);
         if (newFreqHz >= 0)
-        {
-          gen.SetFrequency(REG0, freqHz = newFreqHz);
-          gen.EnableOutput(freqHz > 0);
-        }
+          gen.setFrequency(freqHz = newFreqHz);
       }
       else
       {
-        if (waveType == SINE_WAVE)
-          gen.SetWaveform(REG0, waveType = TRIANGLE_WAVE);
+        if (waveType == AD9833_SINE)
+          gen.setWave(waveType = AD9833_TRIANGLE);
         else
-          gen.SetWaveform(REG0, waveType = SINE_WAVE);
+          gen.setWave(waveType = AD9833_SINE);
       }
+      if (freqHz == 0)
+        midscaleOut();
   
       // Update the display
       lcdUpdate();
@@ -145,18 +154,17 @@ void loop()
       {
         long newFreqHz = freqHz + pow10(pos);
         if (newFreqHz <= MAX_FREQ)
-        {
-          gen.SetFrequency(REG0, freqHz = newFreqHz);
-          gen.EnableOutput(freqHz > 0);
-        }
+          gen.setFrequency(freqHz = newFreqHz);
       }
       else
       {
-        if (waveType == SINE_WAVE)
-          gen.SetWaveform(REG0, waveType = TRIANGLE_WAVE);
+        if (waveType == AD9833_SINE)
+          gen.setWave(waveType = AD9833_TRIANGLE);
         else
-          gen.SetWaveform(REG0, waveType = SINE_WAVE);
+          gen.setWave(waveType = AD9833_SINE);
       }
+      if (freqHz == 0)
+        midscaleOut();
   
       // Update the display
       lcdUpdate();
@@ -167,7 +175,7 @@ void loop()
   if (btn.pressed())
   {
     // Go to next position
-    pos = (++pos) % 8;  // 0..7
+    pos = (pos + 1) % 8;  // 0..7
 
     // Lock the rotary encoder for BTN_PRESS_TIMEOUT_MS
     encLock = true;
