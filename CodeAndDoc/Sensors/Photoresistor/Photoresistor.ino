545fd3d806c3de4e984f95544c60c77c2bfb99ca
Oliver Pfister <oliver.pfister@contaware.com>
2024-11-14 07:19:34 +0000

    More details about photoresistors
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1121 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Sensors/Photoresistor/Photoresistor.ino
+++ CodeAndDoc/Sensors/Photoresistor/Photoresistor.ino
@@ -1,12 +1,38 @@
+/*
+  Test a photoresistor
+  
+  - With no light hitting the photoresistor, the resistance is high.
+    When light hits the photoresistor, the resistance decreases.
+    
+    VCC
+     |
+    .-.
+    | | R
+    | | Photoresistor
+    '-'
+     |
+     o---------o A0
+     |
+    .-.
+    | | R1
+    | | 4.7kΩ
+    '-'
+     |
+    GND
+*/
+const byte V_PIN = A0; // analog pin to read V
+
 void setup()
 {
   Serial.begin(9600);
+  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
+                    // that waits here until the user opens the Serial Monitor!
 }
 
 void loop()
 {
-  int value = analogRead(A0);
-  Serial.print("Value is = ");
+  int value = analogRead(V_PIN);
+  Serial.print("Value is ");
   Serial.println(value);
   delay(1000);
 }
