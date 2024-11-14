71c68f083e4c0ea5ddeb2d078be20d1777435c62
Oliver Pfister <oliver.pfister@contaware.com>
2024-11-14 17:37:38 +0000

    Better description for analog RGB LEDs
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1125 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Display/RGBLed/RGBLed.ino
+++ CodeAndDoc/Display/RGBLed/RGBLed.ino
@@ -1,33 +1,58 @@
-#define BLUE_PIN        3
-#define GREEN_PIN       5
-#define RED_PIN         6
+/*
+  Control an analog RGB led
+
+  - RGB LEDs have three LEDs inside them which share either a 
+    common anode or a common cathode (longest lead).
+
+  - Connect the common cathode to GND, or if it's a common anode,
+    connect it to VCC.
+
+  - Connect each color through a 330Ω resistor to the respective
+    analog pin defined below.
+
+      _---_
+     | RGB |
+    |  LED  |
+    |_______|
+     | | | |
+     B | | |
+       G | R
+        K/A
+*/
+#define BLUE_PIN        3  // PWM pin
+#define GREEN_PIN       5  // PWM pin
+#define RED_PIN         6  // PWM pin
 
 #define DELAY_TIME_MS   15
-#define DELAY_WAIT_MS   1000
+#define DELAY_WAIT_MS   2000
 
 void setup()
 {
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(BLUE_PIN, OUTPUT);
+
 }
 
 void loop()
 {
-  for (int i = 0; i <= 255; ++i)
+  for (int i = 0; i <= 255; i++)
   {
     analogWrite(RED_PIN, 255 - i);
-    //analogWrite(GREEN_PIN, i);
-    analogWrite(BLUE_PIN, i);
+    analogWrite(GREEN_PIN, i);
     delay(DELAY_TIME_MS);
   }
   delay(DELAY_WAIT_MS);
   
-  for (int i = 255; i >= 0; --i)
+  for (int i = 0; i <= 255; i++)
   {
-    analogWrite(RED_PIN, 255 - i);
-    //analogWrite(GREEN_PIN, i);
+    analogWrite(GREEN_PIN, 255 - i);
     analogWrite(BLUE_PIN, i);
+    delay(DELAY_TIME_MS);
+  }
+  delay(DELAY_WAIT_MS);
+
+  for (int i = 0; i <= 255; i++)
+  {
+    analogWrite(BLUE_PIN, 255 - i);
+    analogWrite(RED_PIN, i);
     delay(DELAY_TIME_MS);
   }
   delay(DELAY_WAIT_MS);
