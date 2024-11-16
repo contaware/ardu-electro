3a6dc46c5691a9b9e8995bd4ba9209081116cd7d
Oliver Pfister <oliver.pfister@contaware.com>
2024-11-16 14:03:30 +0000

    No need to wait after Serial.begin() if not printing in setup()
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1144 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Communication/Serial/SerialEcho/SerialEcho.ino
+++ CodeAndDoc/Communication/Serial/SerialEcho/SerialEcho.ino
@@ -15,9 +15,6 @@
 void setup()
 {
   Serial.begin(9600);
-  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
-                    // that waits here until the user opens the Serial Monitor!
-  Serial.println("Test Serial Echo");
 }
 
 void loop()
--- CodeAndDoc/Communication/Serial/SerialPassthrough/SerialPassthrough.ino
+++ CodeAndDoc/Communication/Serial/SerialPassthrough/SerialPassthrough.ino
@@ -7,11 +7,7 @@
 void setup()
 {
   Serial.begin(9600);
-  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
-                    // that waits here until the user opens the Serial Monitor!
   Serial1.begin(9600);
-  Serial.println("Test Serial <-> Serial1");
-  Serial1.println("Test Serial <-> Serial1");
 }
 
 void loop()
