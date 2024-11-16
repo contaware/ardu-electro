3a19574b2a5d0eb1bc49c0650a78ec26d4ba8bb9
Oliver Pfister <oliver.pfister@contaware.com>
2024-11-16 16:14:50 +0000

    Update Serial comments
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1148 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/Sensors/GPS/MKR-GPS-Shield/MKR-GPS-Shield.ino
+++ CodeAndDoc/Sensors/GPS/MKR-GPS-Shield/MKR-GPS-Shield.ino
@@ -21,10 +21,9 @@ unsigned long patienceMillis;
 
 void setup()
 {
-  // Serial init
+  // Init Serial
   Serial.begin(9600);
-  while (!Serial);  // for native USB boards (e.g., Leonardo, Micro, MKR, Nano 33 IoT)
-                    // that waits here until the user opens the Serial Monitor!
+  while (!Serial); // waits here until the user opens the Serial Monitor
   Serial.println("Arduino MKR GPS Shield Test");
 
   // Begin
