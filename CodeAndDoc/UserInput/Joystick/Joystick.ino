7edd9d4f5707d2707757f521a3aaf9113c3b69c2
Oliver Pfister <oliver.pfister@contaware.com>
2024-11-17 15:25:12 +0000

    Update Joystick sketch
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1167 8294280c-25f7-9146-af84-bcdaab6def61

--- CodeAndDoc/UserInput/Joystick/Joystick.ino
+++ CodeAndDoc/UserInput/Joystick/Joystick.ino
@@ -1,9 +1,27 @@
 /*
-  Joystick
+  Analog Joystick
+
+  - On 5V MCUs power it with 5V.
+  
+  - On 3.3V MCUs power it with 3.3V, not with 5V, even if it's marked 
+    as such on the module!
+
+  - The switch shorts to GND when pressed.
+
+  - It uses 10kΩ potentiometers for the positions.
+  
+         VCC                  VCC                 SW
+          |                    |                  |
+         .-.                  .-.                 o
+      Rx | |<---o VRx      Ry | |<---o VRy         \
+    10kΩ | |             10kΩ | |                   \
+         '-'                  '-'                 o
+          |                    |                  |
+         GND                  GND                GND
 */
-const byte SW_PIN = 2;  // digital pin connected to switch output
-const byte X_PIN = 0;   // analog pin connected to X output
-const byte Y_PIN = 1;   // analog pin connected to Y output
+const byte SW_PIN = 2;  // digital pin connected to SW
+const byte X_PIN = A0;  // analog pin connected to VRx
+const byte Y_PIN = A1;  // analog pin connected to VRy
 
 void setup()
 {
@@ -16,14 +34,19 @@ void setup()
 
 void loop()
 {
-  Serial.print("Switch:  ");
+  // Button
+  Serial.print("SW=");
   Serial.print(digitalRead(SW_PIN));
-  Serial.print("\n");
-  Serial.print("X-axis: ");
+  Serial.print(" ");
+
+  // X-axis
+  Serial.print("X=");
   Serial.print(analogRead(X_PIN));
-  Serial.print("\n");
-  Serial.print("Y-axis: ");
+  Serial.print(" ");
+  
+  // Y-axis
+  Serial.print("Y=");
   Serial.println(analogRead(Y_PIN));
-  Serial.print("\n\n");
-  delay(500);
+
+  delay(300);
 }
