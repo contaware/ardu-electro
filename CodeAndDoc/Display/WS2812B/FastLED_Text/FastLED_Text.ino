9eb5367f12e5a0f07784a5e66620151da57d8633
Oliver Pfister <oliver.pfister@contaware.com>
2024-12-01 17:52:00 +0000

    Text output to WS2812B matrix
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1217 8294280c-25f7-9146-af84-bcdaab6def61

---
+++ CodeAndDoc/Display/WS2812B/FastLED_Text/FastLED_Text.ino
@@ -0,0 +1,92 @@
+/*
+  5050 RGB led matrix with integrated WS2812B chips 
+  (SK6812 is a clone of WS2812B)
+
+  - FastLED compared to Adafruit::Neopixel as a backend supports more MCUs
+    and is considered faster, especially if using big tiled displays.
+    On the other side, Adafruit::Neopixel uses less RAM.
+
+  - VDD supply is 5V. You need level-shifters if you want to control 
+    these LEDs with 3.3V levels.
+  
+  - Each LED uses up to 60mA, so make your calcs!
+  
+  - Add a 220Ω - 470Ω resistor between the Arduino digital output pin 
+    and the data input pin to reduce noise on that line.
+
+  - The library supports also tiling matrices to create a big display.
+
+  - Terminology: NeoPixel is Adafruit's brand for individually-addressable 
+                 RGB color pixels and strips based on WS2812B/SK6812.
+*/
+#include <Adafruit_GFX.h>
+#include <Framebuffer_GFX.h>    // by Marc Merlin
+#include <FastLED_NeoMatrix.h>  // by Marc Merlin
+#include <FastLED.h>            // by Daniel Garcia
+
+#define LED_PIN                 2
+#define MATRIX_WIDTH            32
+#define MATRIX_HEIGHT           8
+#define NUM_LEDS                MATRIX_WIDTH*MATRIX_HEIGHT
+CRGB leds[NUM_LEDS];
+
+// Layout flags:
+// - Position of the FIRST LED in the matrix:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT
+// - Proceed order:
+//   NEO_MATRIX_ROWS or NEO_MATRIX_COLUMNS
+// - Arrangement:
+//   NEO_MATRIX_PROGRESSIVE or NEO_MATRIX_ZIGZAG
+FastLED_NeoMatrix matrix = FastLED_NeoMatrix(
+  leds,
+  MATRIX_WIDTH,
+  MATRIX_HEIGHT,
+  NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG);
+
+const uint16_t colors[] =
+{
+  matrix.Color(255, 0, 0), 
+  matrix.Color(0, 255, 0), 
+  matrix.Color(0, 0, 255),
+  matrix.Color(255, 255, 255)
+};
+
+void setup()
+{
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS); // color order: RGB, RBG, GRB, GBR, BRG, BGR
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setTextSize(1);    // 1 is the default 6x8, 2 is 12x16, 3 is 18x24, etc
+  matrix.setRotation(0);    // 0 thru 3 corresponding to 4 cardinal rotations
+  matrix.setBrightness(10); // 0..255 (set according to your power supply current limit)
+  matrix.setTextColor(colors[0]);
+}
+
+int x = matrix.width();
+unsigned int pass = 0;
+
+void loop()
+{
+  matrix.fillScreen(0); // turn off all (pixel in 16-bit '565' RGB format)
+  matrix.setCursor(x, 0);
+  matrix.print("gulp!");
+
+  // Reset x position?
+  if (--x < -matrix.width())
+  {
+    // Reset x position
+    x = matrix.width();
+
+    // Change color
+    if (++pass >= sizeof(colors) / sizeof(colors[0]))
+      pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+
+  // Display
+  matrix.show();
+
+  // For a bigger matrix the refresh rate from show() will
+  // be slow enough that a delay is not necessary.
+  delay(30);
+}
