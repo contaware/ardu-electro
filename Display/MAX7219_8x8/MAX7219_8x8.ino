92c2164a774d7a7e2116fbf5f24a15d0a400efcb
Oliver Pfister <oliver.pfister@contaware.com>
2024-12-05 09:29:40 +0000

    Update MAX7219 8x8-matrix sketch and add two pictures
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1257 8294280c-25f7-9146-af84-bcdaab6def61

--- Display/MAX7219_8x8/MAX7219_8x8.ino
+++ Display/MAX7219_8x8/MAX7219_8x8.ino
@@ -1,32 +1,88 @@
 /*
   MAX7219 8x8 led module
  
-  - VDD supply is 5V.
+  - VDD supply is 5V, operating current with all LEDs ON at
+    maximum brightness is 320mA.
+
+  - The MAX7219 chip drives common-cathode displays. It can
+    drive up to eight 7-segment displays or a 8x8 LED matrix.
+
+  - The MAX7219 is driven by SPI, the LedControl library uses 
+    the shiftOut() function for that.
+  
+  - We can daisy-chain multiple displays to make a larger display,
+    connect the DOUT of the first display to the DIN of the next 
+    display and so forth.
 */
 #include "LedControl.h" // by Eberhard Fahle
 
-LedControl lc=LedControl( 12, // DataIn 
-                          10, // CLK
-                          11, // LOAD(CS) 
-                          1); // we have only a single MAX7219
+LedControl lc = LedControl( 12, // DIN 
+                            10, // CLK
+                            11, // LOAD(CS) 
+                            1); // we have only a single MAX7219
 
-const unsigned long delaytime = 500;
+// Create your own, use:
+// https://jorydotcom.github.io/matrix-emoji/
+byte digit[][8] = {
+  {0x00, 0x3c, 0x24, 0x24, 0x24, 0x24, 0x24, 0x3c},  // 0
+  {0x00, 0x04, 0x0c, 0x14, 0x24, 0x04, 0x04, 0x04},  // 1
+  {0x00, 0x3c, 0x04, 0x04, 0x3c, 0x20, 0x20, 0x3c},  // 2
+  {0x00, 0x3c, 0x04, 0x04, 0x1c, 0x04, 0x04, 0x3c},  // 3
+  {0x00, 0x04, 0x0c, 0x14, 0x3e, 0x04, 0x04, 0x04},  // 4
+  {0x00, 0x3c, 0x20, 0x20, 0x3c, 0x04, 0x04, 0x3c},  // 5
+  {0x00, 0x3c, 0x20, 0x20, 0x3c, 0x24, 0x24, 0x3c},  // 6
+  {0x00, 0x3c, 0x04, 0x04, 0x08, 0x10, 0x10, 0x10},  // 7
+  {0x00, 0x3c, 0x24, 0x24, 0x3c, 0x24, 0x24, 0x3c},  // 8
+  {0x00, 0x3c, 0x24, 0x24, 0x3c, 0x04, 0x04, 0x3c}   // 9
+};
 
 void setup()
 {
   // MAX7219 is in power-saving mode on startup, we have to do a wakeup call
   lc.shutdown(0, false);
   
-  // Set the brightness to a medium values
+  // Set the brightness (0..15)
   lc.setIntensity(0, 8);
-  
-  // Clear the display
-  lc.clearDisplay(0);
 }
 
 void loop()
-{ 
-  lc.setLed(0, 0, 0, true); delay(delaytime); lc.setLed(0, 0, 0, false);
-  lc.setLed(0, 0, 1, true); delay(delaytime); lc.setLed(0, 0, 1, false);
-  lc.setRow(0, 2 , 0b10001000); delay(delaytime); lc.setRow(0, 2, 0);
+{
+  // Control single LEDs
+  lc.clearDisplay(0);
+  for (int row = 0 ; row < 8 ; row++)
+  {
+    for (int col = 0 ; col < 8 ; col++)
+    {
+      lc.setLed(0, row, col, true);
+      delay(30);
+    }
+  }
+  delay(1000);
+
+  // Control rows
+  lc.clearDisplay(0);
+  for (int row = 0 ; row < 8 ; row++)
+  {
+    lc.setRow(0, row, 0b10101010);
+    delay(100);
+  }
+  delay(1000);
+
+  // Control columns
+  lc.clearDisplay(0);
+  for (int col = 0 ; col < 8 ; col++)
+  {
+    lc.setColumn(0, col, 0b10101010);
+    delay(100);
+  }
+  delay(1000);
+
+  // Count from 0..9
+  for (int n = 0 ; n <= 9 ; n++)
+  {
+    for (int row = 0 ; row < 8 ; row++)
+      lc.setRow(0, row, digit[n][row]);
+    delay(500);
+  }
+  delay(1000);
 }
