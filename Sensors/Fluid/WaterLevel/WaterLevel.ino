6fc7b23f985e9838479d25d108c536e4daf7474b
Oliver Pfister <oliver.pfister@contaware.com>
2024-12-05 14:22:34 +0000

    Update Water Level Sensor sketch and add two pictures
    git-svn-id: file:///D/data/svnrepos/Arduino/trunk@1258 8294280c-25f7-9146-af84-bcdaab6def61

--- Sensors/Fluid/WaterLevel/WaterLevel.ino
+++ Sensors/Fluid/WaterLevel/WaterLevel.ino
@@ -1,5 +1,13 @@
 /*
   Water Level Detection Sensor
+
+  - VDD supply is 3.3V - 5V.
+
+  - The sensor traces resistance varies inversely with the depth of 
+    immersion in water. The common collector circuit generates an output 
+    voltage proportional to the current flowing into the transistor 
+    base, thus when the sensor resistance lowers, the output voltage 
+    increases.
 */
 const byte SIGNAL_PIN = A0;
 
