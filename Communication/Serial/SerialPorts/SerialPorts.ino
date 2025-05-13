ebc2dc7e6915feb19b06f6ab7ca76261d0ee35b1
Oliver Pfister <oliver.pfister@contaware.com>
2025-05-13 22:27:30 +0200

    Update SerialPorts sketch to support ESP32's Serial0

--- Communication/Serial/SerialPorts/SerialPorts.ino
+++ Communication/Serial/SerialPorts/SerialPorts.ino
@@ -2,37 +2,54 @@
   Serial Ports
   
   Some boards have several Serial Ports:
-  - Serial is the main port for debug messages.
-  - SerialUSB is the native USB port, also for debug messages.
-  - Serial1 is the first additional hardware serial port.
-  - Serial2 is another hardware serial port.
-  - Serial3 is a third hardware serial port.
+  - Serial is the port used for debug prints.
+    Note: Serial is sometimes a hardware serial and other times a serial 
+    over USB. Additionally it may be an alias for one of the following ports.
+  - SerialUSB is sometimes defined for the serial communication over USB.
+  - Serial0 is the hardware serial port 0 for ESP32.
+  - Serial1 is the hardware serial port 1.
+  - Serial2 is the hardware serial port 2.
+  - Serial3 is the hardware serial port 3.
 */
 
 // Test the various Serial ports by uncommenting them
 #define MySerial        Serial
 //#define MySerialUSB     SerialUSB
+//#define MySerial0       Serial0
 #define MySerial1       Serial1
 //#define MySerial2       Serial2
 //#define MySerial3       Serial3
 
+// For ESP32 we define the GPIOs/pins of Serial1 & Serial2
+#define MY_RX1          21  // D10 or 21
+#define MY_TX1          18  // D9  or 18
+#define MY_RX2          17  // D8  or 17
+#define MY_TX2          10  // D7  or 10
+
 void setup()
 {
-  // Init LED
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  // Init Serial(s)
 #ifdef MySerial
   MySerial.begin(9600);
 #endif
 #ifdef MySerialUSB
   MySerialUSB.begin(9600);
 #endif
+#ifdef MySerial0
+  MySerial0.begin(9600);
+#endif
 #ifdef MySerial1
-  MySerial1.begin(9600);
+  #if defined(ARDUINO_ARCH_ESP32)
+    MySerial1.begin(9600, SERIAL_8N1, MY_RX1, MY_TX1);
+  #else
+    MySerial1.begin(9600);
+  #endif
 #endif
 #ifdef MySerial2
-  MySerial2.begin(9600);
+  #if defined(ARDUINO_ARCH_ESP32)
+    MySerial2.begin(9600, SERIAL_8N1, MY_RX2, MY_TX2);
+  #else
+    MySerial2.begin(9600);
+  #endif
 #endif
 #ifdef MySerial3
   MySerial3.begin(9600);
@@ -41,7 +58,6 @@ void setup()
 
 void loop()
 {
-  // Count-up
   static unsigned long count = 0;
 #ifdef MySerial
   MySerial.print("Serial: ");
@@ -50,6 +66,10 @@ void loop()
 #ifdef MySerialUSB
   MySerialUSB.print("SerialUSB: ");
   MySerialUSB.println(count);
+#endif
+#ifdef MySerial0
+  MySerial0.print("Serial0: ");
+  MySerial0.println(count);
 #endif
 #ifdef MySerial1
   MySerial1.print("Serial1: ");
@@ -64,10 +84,5 @@ void loop()
   MySerial3.println(count);
 #endif
   count++;
-
-  // Blink
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(1000);
 }
